55b2ac213d79a6499f1aef221e9c869948380854
William Flora <William.Flora@williamf.dev>
2022-11-11 09:42:54 -0500

    Lib class finished
    Added all necessary attributes to the library class

--- libManager.py
+++ libManager.py
@@ -1,15 +1,18 @@
 # Library class with library name, booklist, and booklender dictionary
 class Library:
+
     def init(self, booksList, name):
         self.booksList = booksList
         self.name = name
         self.lendDict = {}
 
+    # displays books available to checkout
     def displayBooks(self):
         print(f'We have following books in our library:{self.name}')
         for book in self.booksList:
             print(book)
 
+    # Adds book to database if not already present
     def addBook(self, book):
         if book in booksList:
             print('Book already exists')
@@ -17,6 +20,8 @@ class Library:
             self.booksList.append(book)
             print('Book added')
 
+    # Checkout book and update database, provide name of who checked out a book, and apologize for not having a book
+    # in the library
     def lendBook(self, book, user):
         if book in booksList:
             if book not in self.lendDict.keys():
@@ -27,4 +32,10 @@ class Library:
         else:
             print("I'm sorry, we don't have this book in our library.")
 
-    
+    #return book and update database, identify if book isn't from this library.
+    def returnBook(self, book):
+        if book in self.lendDict.keys():
+            self.lendDict.pop(book)
+            print('Book returned successfully. Thank you.')
+        else:
+            print('The book does not exist in the book lending database.')
